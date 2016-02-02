9ad052819dea162b24b3ecabef28cdcec32c6b52
pollitosabroson <karnal02@gmail.com>
2016-02-01 22:27:05 -0600

    Integrate new URLS

--- src/idneo/settings/__init__.py
+++ src/idneo/settings/__init__.py
@@ -58,8 +58,9 @@ WSGI_APPLICATION = 'idneo.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'idneo',
+        'USER': 'vagrant'
     }
 }
 
--- src/idneo/urls.py
+++ src/idneo/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'idneo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(
+        r'^$',
+        'books.views.home', name='home'),
+    url(r'^book', include('books.urls'))
 )
